ac2b5749ce7e21da7bf9667350b4f15e37cc0d14
stonevessel <157413052+stonevessel@users.noreply.github.com>
2024-01-25 12:52:51 -0500

    updated the end message

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,11 @@ public class Main {
 
         int[] highs = {45,29,10,22,41,28,33};
         double[] precipitation={.95,.60,.25,.05,0,.75,.9};
-        String[] weekDays={"monday","tuesday","wednesday","thursday","friday","saturday","sunday"};
+        String[] weekDays={"sunday","monday","tuesday","wednesday","thursday","friday","saturday"};
 
         for (int i = 0; i < highs.length; i++) {
             if(highs[i]<= 32 && precipitation[i]>= .5 ){
-                System.out.println("For "+ weekDays[i]+" the high is " +  highs[i]+" and the precipitation is " + precipitation[i]*100+"% its mostly going to snow yeah!!");
+                System.out.println(weekDays[i]+" are the days it's going to snow.");
             }
         }
 
